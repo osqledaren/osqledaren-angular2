3234695bd0e8772d314d50b86191a1488261a535
tedyw <tedy@kth.se>
2016-10-10 11:20:54 +0200

    Added notifications and gitignore rules.

--- gulpfile.js
+++ gulpfile.js
@@ -2,54 +2,59 @@
  * Prototype gulpfile
  */
 
-  var gulp = require('gulp');
+// Load plugins
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	minifycss = require('gulp-minify-css'),
+	rename = require('gulp-rename'),
+	notify = require('gulp-notify'),
+	appDefaults = {
+		cssDir: './assets/css/', // path to compiled css
+		scssDir: './src/scss/', // path to styles
+		jsDir: './assets/js', // path to js
+		nodeModulesDir: "./node_modules/" // path to node modules
+	},
+	browserSync = require('browser-sync').create();
 
-  var gutil = require('gulp-util');
-  var sass = require('gulp-sass');
-  var prefix = require('gulp-autoprefixer');
-  var minifycss = require('gulp-minify-css');
-  var browserSync = require('browser-sync').create();
+// Styles
+gulp.task('styles', function () {
+	return gulp.src(appDefaults.scssDir + '**/*.scss')
+		.pipe(sass({
+			style: 'compressed',
+			includePaths: [appDefaults.nodeModulesDir + 'bootstrap/scss', appDefaults.scssDir]
+		})).on('error', notify.onError(function (error) {
+			return 'An error occurred while compiling sass.\nLook in the console for details.\n' + error;
+		}))
+		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(minifycss())
+		.pipe(gulp.dest(appDefaults.cssDir))
+		.pipe(notify({message: 'Styles task complete'}))
+		.pipe(browserSync.reload({stream: true}));
+});
 
-  gulp.task('sass', function (){
-    gulp.src(['./src/scss/*.scss', '!./src/scss/_variables.scss'])
-    .pipe(sass({
-      includePaths: ['./node_modules/bootstrap/scss', './src/scss'],
-      outputStyle: 'expanded'
-    }))
-    .on('error', function (err) {
-      gutil.log(err);
-      this.emit('end');
-    })
-    .pipe(prefix(
-      "last 1 version", "> 1%", "ie 8", "ie 7"
-      ))
-    .pipe(gulp.dest('./assets/css'))
-    .pipe(minifycss())
-    .pipe(gulp.dest('./assets/css'))
-    .pipe(gulp.dest('./assets/css')).on('error', gutil.log)
-    .pipe(browserSync.reload({stream: true}));
-  });
+gulp.task('copyjs', function () {
+	gulp.src(appDefaults.nodeModulesDir + 'bootstrap/dist/js/bootstrap.min.js')
+		.pipe(gulp.dest(appDefaults.jsDir));
 
-  gulp.task('copyjs', function() {
-    gulp.src('./node_modules/bootstrap/dist/js/bootstrap.min.js')
-    .pipe(gulp.dest('./assets/js'));
+	gulp.src(appDefaults.nodeModulesDir + 'jquery/dist/jquery.min.js')
+		.pipe(gulp.dest(appDefaults.jsDir));
 
-    gulp.src('./node_modules/jquery/dist/jquery.min.js')
-    .pipe(gulp.dest('./assets/js'));
+	gulp.src(appDefaults.nodeModulesDir + 'tether/dist/js/tether.min.js')
+		.pipe(gulp.dest(appDefaults.jsDir));
+});
 
-    gulp.src('./node_modules/tether/dist/js/tether.min.js')
-    .pipe(gulp.dest('./assets/js'));
-  });
+gulp.task('serve', function () {
+	browserSync.init({
+		server: './'
+	});
 
-  gulp.task('serve', function() {
-    browserSync.init({
-      server: {
-        baseDir: "./"
-      }
-    });
+	gulp.watch(appDefaults.scssDir + '**/*.scss', function (event) {
+		console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+		gulp.run('styles');
+	});
+	gulp.watch('./*.html').on('change', browserSync.reload);
+});
 
-    gulp.watch('./src/scss/**/*.scss', ['sass']);
-    gulp.watch('./*.html').on('change', browserSync.reload);
-  });
-
-  gulp.task('default', ['serve', 'copyjs']);
+gulp.task('default', ['copyjs', 'styles', 'serve']);
