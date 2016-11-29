f09d4f325be5d558c9e30401a3fecc28fcf5cedc
Tedy Warsitha <tedy@kth.se>
2016-11-29 16:58:57 +0100

    Fixed masonry issue.

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-var $article_grid = $('.print-grid-container');
+var $print_grid = $('.print-grid-container');
 //initializing isotope
-$article_grid.imagesLoaded( function() {
+$print_grid.imagesLoaded( function() {
   	// init Isotope after all images have loaded
-	$article_grid.isotope({
+	$print_grid.isotope({
 		itemSelector: '.print-grid-item',
 		percentPosition: true,
 		masonry: {
 		  	// use element for size control
 			columnWidth: '.print-grid-sizer'
+		}
+	})
+});
+
+var $article_grid = $('.article-grid-container');
+//initializing isotope
+$article_grid.imagesLoaded(function () {
+	// init Isotope after all images have loaded
+	$article_grid.isotope({
+		itemSelector: '.article-grid-item',
+		percentPosition: true,
+		masonry: {
+			// use element for size control
+			columnWidth: '.article-grid-sizer'
 		}
 	})
 });
