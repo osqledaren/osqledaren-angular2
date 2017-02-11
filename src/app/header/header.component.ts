468d6f2e9b99a633fa3c7fa0e5ea763fd4e628da
Tedy Warsitha <tedy@kth.se>
2017-02-11 03:27:20 +0100

    Started working on auto-close timer.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,46 +1,45 @@
-import {Component, OnInit, HostListener, ElementRef} from "@angular/core";
+import {Component, OnInit, HostListener, ElementRef, OnDestroy} from "@angular/core";
 import {IMenuList} from "../shared/interface/menu.interface";
 import {NavigationService} from "../navigation.service";
+import 'rxjs/add/operator/throttleTime';
+import 'rxjs/add/observable/fromEvent';
+import {Observable, Subscription} from "rxjs";
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
     public mainMenu: IMenuList;
     public secondaryMenu: IMenuList;
     public isCollapsed: boolean = true;
     private visible = true;
     private lastScrollPos = 0;
+    private autoCloseTimer: Subscription;
+    private autoCloseTime: number = 0;
+    private inside: boolean;
 
     constructor(private navigation: NavigationService, private elementRef: ElementRef) {
         this.mainMenu = navigation.getNav('main-nav');
         this.secondaryMenu = navigation.getNav('secondary-nav');
     }
 
-    @HostListener('window:scroll', ['$event'])
-    private onScroll(event) {
-        let scrollTop = document.body.scrollTop;
-
-        if (scrollTop < 100 || (scrollTop < this.lastScrollPos && scrollTop <= document.body.offsetHeight - window.outerHeight - 50)) {
-            this.showMenu();
-        } else {
-            this.hideMenu();
-        }
-
-        this.lastScrollPos = scrollTop;
-    }
-
     @HostListener('document:click', ['$event.target'])
     private onClick(targetElement) {
         let clickedInside = this.elementRef.nativeElement.contains(targetElement);
-        let scrollTop = document.body.scrollTop;
-
         if (!clickedInside) {
             this.isCollapsed = true;
         }
+    }
+
+    private mouseOver() {
+        this.inside = true;
+    }
+
+    private mouseLeave(){
+        this.inside = false;
     }
 
     private hideMenu() {
@@ -53,7 +52,48 @@ export class HeaderComponent implements OnInit {
         this.isCollapsed = this.visible && !this.isCollapsed ? false : true;
     }
 
+    private ToggleSubMenu() {
+
+        this.isCollapsed = !this.isCollapsed;
+
+        /*if (!this.isCollapsed) {
+
+            let time: number = 0;
+
+            this.autoCloseTimer = Observable.timer(0, 1000).subscribe(
+                () => {
+                    if(!this.inside && this.autoCloseTime > 3){
+                        this.ToggleSubMenu();
+                    } else {
+                        this.autoCloseTime += 1;
+                    }
+                }
+            );
+        } else {
+            this.autoCloseTimer = new Subscription;
+            this.autoCloseTime = 0;
+        }*/
+    }
+
     ngOnInit() {
+        // throttle scroll event
+        Observable.fromEvent(window, 'scroll')
+            .throttleTime(100)
+            .subscribe(() => {
+                let scrollTop = document.body.scrollTop;
+
+                if (scrollTop < 100 || (scrollTop < this.lastScrollPos && scrollTop <= document.body.offsetHeight - window.outerHeight - 50)) {
+                    this.showMenu();
+                } else {
+                    this.hideMenu();
+                }
+
+                this.lastScrollPos = scrollTop;
+            });
+    }
+
+    ngOnDestroy() {
+        this.autoCloseTimer.unsubscribe();
     }
 
 }
