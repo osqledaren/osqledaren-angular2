f72fb4677d7fa9dcdb0a3f61a075a98310a755a2
JOhohannes Loor <johannes.loor@gmail.com>
2019-05-09 15:13:46 +0200

    Styled and fixed so the btn disbles

--- src/app/site/tip/tip.component.ts
+++ src/app/site/tip/tip.component.ts
@@ -12,7 +12,10 @@ import {TipSubmitService} from '../tip-submit.service';
 
 })
 export class TipComponent extends UILoadableComponent {
-
+  private tipSubmit = null;
+  private message = null;
+  private mail = 'Ingen mail angavs';
+  private tip = null;
   constructor(loaderService: UIViewLoaderService, private archiveService: ArchiveService, tipSubmit: TipSubmitService) {
     super(loaderService);
     this.tipSubmit = tipSubmit;
@@ -26,6 +29,13 @@ export class TipComponent extends UILoadableComponent {
   //Sets the message from the user
   messageChanged(value: string) {
     this.message = value;
+    let sendBtn = document.getElementById('sendBtn')
+    if(this.message){
+      sendBtn.classList.remove('disabled')
+    }
+    else{
+      sendBtn.classList.add('disabled')
+    }
   }
 
   //Sets the mail from the user
@@ -35,17 +45,19 @@ export class TipComponent extends UILoadableComponent {
 
   //Sends the message to slack and shows the thank you page
   onClickMe() {
-    this.tip = this.message + '\n\nMail: ' + this.mail
-    this.tipSubmit.submitToSlack(this.tip);
+    if(this.message){
+      this.tip = this.message + '\n\nMail: ' + this.mail
+      this.tipSubmit.submitToSlack(this.tip);
 
-    let form = document.getElementById('tipForm');
-    form.style.display = 'none';
-    let sentMessage = document.getElementById('tipSent');
-    sentMessage.style.display = 'block';
+      let form = document.getElementById('tipForm');
+      form.style.display = 'none';
+      let sentMessage = document.getElementById('tipSent');
+      sentMessage.style.display = 'block';
 
-    this.message = '';
-    this.mail = '';
-    this.tip = '';
+      this.message = null;
+      this.mail = null;
+      this.tip = null;
+    }
   }
 
 
